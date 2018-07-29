fec2b24f03c3daf8aecd81a1f451ac1af20d7475
Ayu Iha <17oa03@gmail.com>
2018-07-29 17:54:15 +0900

    簡単にトップページを作成しました、内部的な処理はまだ作成していません。

---
+++ resources/views/tops/index.blade.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width,initial-scale=1">
+<meta name="keywords" content="" />
+<meta name="description" content="サンプル動画を紹介するアダルトサイトです。" />
+<title>まるきん</title>
+
+<!-- Bootstrap4 CDN CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+<!--[if IE]>
+<script type="text/javascript" src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+
+</head>
+<body>
+
+    <!-- jquery CDN JS -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+    <!-- Bootstrap4 CDN JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+    <div class="container-fluid">
+
+        <!-- ナビゲーション -->
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+
+            <!-- トグル -->
+            <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#Navber" aria-controls="Navber" aria-expanded="false" aria-label="ナビゲーションの切替">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <!-- ブランド名  -->
+            <a class="navbar-brand" href="#">まるきん</a>
+
+            <div class="collapse navbar-collapse" id="Navber">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item dropdown">
+                        <a href="#" class="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        ジャンル
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="#">巨乳</a>
+                            <a class="dropdown-item" href="#">素人</a>
+                            <a class="dropdown-item" href="#">アナル</a>
+                        </div>
+
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a href="#" class="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        ランキング
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="#">視聴数</a>
+                            <a class="dropdown-item" href="#">動画の長さ</a>
+                            <a class="dropdown-item" href="#">女性に人気</a>
+                        </div>
+
+                    </li>
+                </ul>
+            <form class="form-inline my-2 my-lg-0">
+                <input type="search" class="form-control mr-sm-2" placeholder="AV女優名" aria-label="検索...">
+                <button type="submit" class="btn btn-outline-success my-2 my-sm-0">検索</button>
+            </form>
+            </div>
+        </nav>
+
+        <!-- サンプル動画1 -->
+        <div class="row">
+            <div class="col-sm">
+                <div class="code mx-auto" style="width: 50%;">
+                    <div class="bs-example" data-example-id="responsive-embed-16by9-iframe-youtube">
+                        <div class="embed-responsive embed-responsive-16by9">
+                            <iframe class="embed-responsive-item" src="//www.youtube.com/embed/zpOULjyy-n8?rel=0" allowfullscreen></iframe>
+                        </div>
+                    </div>
+                    <p>動画タイトル</p>
+                    <p>説明文</p>
+                    <p>出演者</p>
+                    <p>値段</p>
+                </div>
+            </div>
+        </div>
+
+        <!-- サンプル動画2 -->
+        <div class="row">
+            <div class="col-sm">
+                <div class="code mx-auto" style="width: 50%;">
+                    <div class="bs-example" data-example-id="responsive-embed-16by9-iframe-youtube">
+                        <div class="embed-responsive embed-responsive-16by9">
+                            <iframe class="embed-responsive-item" src="//www.youtube.com/embed/zpOULjyy-n8?rel=0" allowfullscreen></iframe>
+                        </div>
+                    </div>
+                    <p>動画タイトル</p>
+                    <p>説明文</p>
+                    <p>出演者</p>
+                    <p>値段</p>
+                </div>
+            </div>
+        </div>
+
+        <!-- サンプル動画3 -->
+        <div class="row">
+            <div class="col-sm">
+                <div class="code mx-auto" style="width: 50%;">
+                    <div class="bs-example" data-example-id="responsive-embed-16by9-iframe-youtube">
+                        <div class="embed-responsive embed-responsive-16by9">
+                            <iframe class="embed-responsive-item" src="//www.youtube.com/embed/zpOULjyy-n8?rel=0" allowfullscreen></iframe>
+                        </div>
+                    </div>
+                    <p>動画タイトル</p>
+                    <p>説明文</p>
+                    <p>出演者</p>
+                    <p>値段</p>
+                </div>
+            </div>
+        </div>
+</body>
+</html>
